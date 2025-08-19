8161b64cc2c093e7b8ca03aa7189ea0552a0a55b
lehoangphuc747 <lphc664@yahoo.com.vn>
2025-08-19 14:46:03 +0700

    feat(ui): nâng cấp style 2 nút Blog & Giới thiệu (Tailwind)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lehoangphuc747.github.io',
-	integrations: [mdx(), sitemap()],
-	image: {
-		service: {
-			entrypoint: 'astro/assets/services/sharp',
-		},
-	},
+  site: 'https://lehoangphuc747.github.io',
+  integrations: [mdx(), sitemap(), react()],
+
+  image: {
+      service: {
+          entrypoint: 'astro/assets/services/sharp',
+      },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
